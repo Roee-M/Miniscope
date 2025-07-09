4b03ae6a7b73ce5a0b7acd17b49488e1277506a5
orslup <orslup@gmail.com>
2025-07-09 20:00:14 +0300

    ESP ADC Loopback working

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,14 +1,52 @@
-// Simple Blink Example for ESP32-S3 using GPIO8
 #include <Arduino.h>
-#define LED_PIN 8
+#define LED_PIN 48
+#define LB_ADC_PIN 4  // GPIO4 for analogRead
+#define LB_OUT_PIN 5  // GPIO5 outputs simulated signal
+
+int val = 1;
 
 void setup() {
+  Serial.begin(115200); 
   pinMode(LED_PIN, OUTPUT);
+  pinMode(LB_OUT_PIN, OUTPUT);
+  digitalWrite(LB_OUT_PIN, HIGH);  // Simulate signal
+  analogReadResolution(12);
+  Serial.print("Finished setup");
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH); // Turn LED on
-  delay(1000);                  // Wait 500ms
+  digitalWrite(LED_PIN, HIGH); // Turn LED on   
+
+  digitalWrite(LB_OUT_PIN, HIGH);  // simulate signal
+  delay(1);
+  val = analogRead(LB_ADC_PIN);
+  Serial.print("HIGH: ");
+  Serial.println(val);
+
   digitalWrite(LED_PIN, LOW);  // Turn LED off
-  delay(1000);                  // Wait 500ms
+
+  digitalWrite(LB_OUT_PIN, LOW);  // simulate signal
+  delay(1);
+  val = analogRead(LB_ADC_PIN);
+  Serial.print("LOW: ");
+  Serial.println(val);
+
+  delay(100);
 }
+
+// TODO
+// Other ESP:
+//   Connect LB
+  //? read_ADC() (-> API, implement as actual ADC SPI reads later)
+  //? store in ESP memory
+  //? Add trigger functionality
+  //? send over Wifi/USB/mail
+  //? simple server to plot incoming data
+
+// Miniscope itself:  
+  //? implement as actual ADC SPI reads
+  //? Print the reads (Serial)
+
+// Frontend:
+  //? Upgrade debug server to be able to display and plot the data over the phone/web
+  //? Add trigger functionality via phone
